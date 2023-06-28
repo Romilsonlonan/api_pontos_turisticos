17dc1c2c15111f3f661309fa8c8d5c15452f69f4
Romilsonlonan <romilsonlonan@gmail.com>
2023-06-28 01:05:57 -0300

    config serializers e viewset comentarios

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -2,6 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
 from .serializers import AtracaoSerializer
 
-class AtracoesViewSet(ModelViewSet):
+class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -5,15 +5,16 @@ from django.urls import path
 #pegar na documentação
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
-
+from comentarios.api.viewsets import ComentarioViewSet
 
 #Configurando as rotas de acesso aos endpoints
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
 
 
 
